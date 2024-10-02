9ea73ea23ddf003247c02cc9f9d180166b2c36d4
lucasvil4r <lucasvilar-celestino@hotmail.com>
2024-10-02 16:23:23 -0300

    feature: criando outro controller para inserir user via APIs

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\BinaryTreeController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\ApiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [BinaryTreeController::class, 'index'])->name('binarytree.index');
 Route::get('/binarytree', [BinaryTreeController::class, 'index'])->name('binarytree.index');
-Route::post('/register', [BinaryTreeController::class, 'registerUser'])->name('register.user');
+Route::post('/register', [BinaryTreeController::class, 'registerUser'])->name('binarytree.register');
+
+Route::post('/api/register', [ApiController::class, 'registerUser'])->name('api.register');
+Route::post('/api/user/{userId}/add-points', [ApiController::class, 'addPoints'])->name('api.addPoints');
+Route::get('/api/user/{userId}/points-summary', [ApiController::class, 'getPointsSummary'])->name('api.pointsSummary');
